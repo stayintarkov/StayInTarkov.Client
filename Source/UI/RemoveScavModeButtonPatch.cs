7df7d858888ebc239e935a0df4183fa3fe901124
TehFl0w <34872043+TehFl0w@users.noreply.github.com>
2023-11-19 04:39:58 +0100

    Update RemoveScavModeButtonPatch.cs
    Updated logic for pmc-element-centering

--- Source/UI/RemoveScavModeButtonPatch.cs
+++ Source/UI/RemoveScavModeButtonPatch.cs
@@ -21,13 +21,18 @@ namespace StayInTarkov.UI
         {
             var selecttxt = GameObject.Find("SideSelectionCaption");
             selecttxt.active = false;
+
             var savages = GameObject.Find("Savage");
             savages.active = false;
+
             var pmcdesc = GameObject.Find("Description");
             pmcdesc.active = false;
+
             var pmcs = GameObject.Find("PMCs");
-            pmcs.transform.localScale = new Vector3( (float)1.3, (float)1.3, (float)1.0);
-            pmcs.transform.position = new Vector3((Screen.width / 2) - 293, (Screen.height / 2) - 130, 0);
+            var pmccanvas = GameObject.Find("PMCPlayerMV");
+
+            pmcs.transform.localScale = new Vector3(1.1f, 1.1f, 1.0f);
+            pmcs.transform.position = new Vector3((Screen.width / 2) - (pmccanvas.RectTransform().rect.width / 2), (Screen.height / 2) - (Screen.height/10) , 0);
         }
     }
 }
