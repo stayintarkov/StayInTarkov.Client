9620efc096967a3cd4e48c1010cc520115a7722f
JiayingChen <admin@eve-info.net>
2023-12-04 18:24:37 +0800

    Add an using

--- Source/Networking/AkiBackendCommunication.cs
+++ Source/Networking/AkiBackendCommunication.cs
@@ -18,6 +18,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
