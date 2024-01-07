0b74f39cc2f6c6fbcaefc18d1524cd081619709d
Paulov <pargsoft@hotmail.com>
2024-01-07 11:26:50 +0000

    Create PlayerFactory with methods to create quest,stats and achievement controllers

---
+++ Source/Coop/Players/PlayerFactory.cs
@@ -0,0 +1,34 @@
+﻿using EFT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine.Profiling;
+
+namespace StayInTarkov.Coop.Players
+{
+    internal class PlayerFactory
+    {
+        public static QuestController GetQuestController(EFT.Profile profile, InventoryController inventoryController)
+        {
+            var questController = new QuestController(profile, inventoryController, null, false);
+            questController.Init();
+            questController.Run();
+            return questController;
+        }
+
+        public static IStatisticsManager GetStatisticsManager(EFT.Profile profile, InventoryController inventoryController)
+        {
+            return new CoopPlayerStatisticsManager(profile);
+        }
+
+        public static AchievementControllerClass GetAchievementController(EFT.Profile profile, InventoryController inventoryController)
+        {
+            var aController = new AchievementControllerClass(profile, inventoryController, StayInTarkovHelperConstants.BackEndSession, true);
+            aController.Init();
+            aController.Run();
+            return aController;
+        }
+    }
+}
