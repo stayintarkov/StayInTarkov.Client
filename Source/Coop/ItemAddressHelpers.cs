cb9307b9f5f1c69615af03efec77ebcbc17b245d
Paulov <pargsoft@hotmail.com>
2024-01-29 08:03:41 +0000

    Add Stack Slot helper

--- Source/Coop/ItemAddressHelpers.cs
+++ Source/Coop/ItemAddressHelpers.cs
@@ -5,8 +5,9 @@ namespace StayInTarkov.Coop
 {
     public static class ItemAddressHelpers
     {
-        private const string DICTNAMES_SlotItemAddressDescriptor = "sitad";
-        private const string DICTNAMES_GridItemAddressDescriptor = "grad";
+        private static string DICTNAMES_SlotItemAddressDescriptor { get; } = "sitad";
+        private static string DICTNAMES_GridItemAddressDescriptor { get; } = "grad";
+        private static string DICTNAMES_StackSlotItemAddressDescriptor { get; } = "ssad";
 
         public static void ConvertItemAddressToDescriptor(ItemAddress location, ref Dictionary<string, object> dictionary)
         {
@@ -28,16 +29,65 @@ namespace StayInTarkov.Coop
 
                 dictionary.Add(DICTNAMES_SlotItemAddressDescriptor, slotItemAddressDescriptor);
             }
+            else if (location is StackSlotItemAddress StackSlotItemAddress)
+            {
+                StackSlotItemAddressDescriptor stackSlotItemAddressDescriptor = new();
+                stackSlotItemAddressDescriptor.Container = new();
+                stackSlotItemAddressDescriptor.Container.ContainerId = location.Container.ID;
+                stackSlotItemAddressDescriptor.Container.ParentId = location.Container.ParentItem != null ? location.Container.ParentItem.Id : null;
+                dictionary.Add(DICTNAMES_StackSlotItemAddressDescriptor, stackSlotItemAddressDescriptor);
+            }
+        }
+
+        public static void ConvertItemAddressToDescriptor(ItemAddress location
+            , ref Dictionary<string, object> dictionary
+            , out GridItemAddressDescriptor gridItemAddressDescriptor
+            , out SlotItemAddressDescriptor slotItemAddressDescriptor
+            , out StackSlotItemAddressDescriptor stackSlotItemAddressDescriptor
+            )
+        {
+            gridItemAddressDescriptor = null;
+            slotItemAddressDescriptor = null;
+            stackSlotItemAddressDescriptor = null;
+
+            if (location is GridItemAddress gridItemAddress)
+            {
+                gridItemAddressDescriptor = new();
+                gridItemAddressDescriptor.Container = new ContainerDescriptor();
+                gridItemAddressDescriptor.Container.ContainerId = location.Container.ID;
+                gridItemAddressDescriptor.Container.ParentId = location.Container.ParentItem != null ? location.Container.ParentItem.Id : null;
+                gridItemAddressDescriptor.LocationInGrid = gridItemAddress.LocationInGrid;
+                dictionary.Add(DICTNAMES_GridItemAddressDescriptor, gridItemAddressDescriptor);
+            }
+            else if (location is SlotItemAddress slotItemAddress)
+            {
+                slotItemAddressDescriptor = new();
+                slotItemAddressDescriptor.Container = new ContainerDescriptor();
+                slotItemAddressDescriptor.Container.ContainerId = location.Container.ID;
+                slotItemAddressDescriptor.Container.ParentId = location.Container.ParentItem != null ? location.Container.ParentItem.Id : null;
+
+                dictionary.Add(DICTNAMES_SlotItemAddressDescriptor, slotItemAddressDescriptor);
+            }
+            else if (location is StackSlotItemAddress StackSlotItemAddress)
+            {
+                stackSlotItemAddressDescriptor = new();
+                stackSlotItemAddressDescriptor.Container = new();
+                stackSlotItemAddressDescriptor.Container.ContainerId = location.Container.ID;
+                stackSlotItemAddressDescriptor.Container.ParentId = location.Container.ParentItem != null ? location.Container.ParentItem.Id : null;
+                dictionary.Add(DICTNAMES_StackSlotItemAddressDescriptor, stackSlotItemAddressDescriptor);
+            }
         }
 
         public static void ConvertDictionaryToAddress(
             Dictionary<string, object> dict,
             out GridItemAddressDescriptor gridItemAddressDescriptor,
-            out SlotItemAddressDescriptor slotItemAddressDescriptor
+            out SlotItemAddressDescriptor slotItemAddressDescriptor,
+            out StackSlotItemAddressDescriptor stackSlotItemAddressDescriptor
             )
         {
             gridItemAddressDescriptor = null;
             slotItemAddressDescriptor = null;
+            stackSlotItemAddressDescriptor = null;
             if (dict.ContainsKey(DICTNAMES_GridItemAddressDescriptor))
             {
                 gridItemAddressDescriptor = StayInTarkovHelperConstants.SITParseJson<GridItemAddressDescriptor>(dict[DICTNAMES_GridItemAddressDescriptor].ToString());
@@ -46,6 +96,11 @@ namespace StayInTarkov.Coop
             if (dict.ContainsKey(DICTNAMES_SlotItemAddressDescriptor))
             {
                 slotItemAddressDescriptor = StayInTarkovHelperConstants.SITParseJson<SlotItemAddressDescriptor>(dict[DICTNAMES_SlotItemAddressDescriptor].ToString());
+            }
+
+            if (dict.ContainsKey(DICTNAMES_StackSlotItemAddressDescriptor))
+            {
+                stackSlotItemAddressDescriptor = StayInTarkovHelperConstants.SITParseJson<StackSlotItemAddressDescriptor>(dict[DICTNAMES_StackSlotItemAddressDescriptor].ToString());
             }
         }
     }
