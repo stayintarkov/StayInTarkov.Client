2275ade7ae0f69c0ae0f3d2ebc397a99165e7db5
Paulov <pargsoft@hotmail.com>
2024-02-26 21:22:38 +0000

    Move Pickup packet

--- Source/Coop/Player/FirearmControllerPatches/FirearmController_Pickup_Patch.cs
+++ Source/Coop/Player/FirearmControllerPatches/FirearmController_Pickup_Patch.cs
@@ -1,5 +1,6 @@
 ﻿using StayInTarkov.Coop.Components.CoopGameComponents;
 using StayInTarkov.Coop.NetworkPacket;
+using StayInTarkov.Coop.NetworkPacket.Player.Weapons;
 using StayInTarkov.Networking;
 using System;
 using System.Collections.Generic;
@@ -18,9 +19,6 @@ namespace StayInTarkov.Coop.Player.FirearmControllerPatches
             return method;
         }
 
-        public static List<string> CallLocally
-            = new();
-
 
         [PatchPrefix]
         public static bool PrePatch(
@@ -28,25 +26,7 @@ namespace StayInTarkov.Coop.Player.FirearmControllerPatches
             , EFT.Player ____player
             )
         {
-            //Logger.LogInfo("FirearmController_Pickup_Patch.PrePatch");
-
-            if (CoopGameComponent.GetCoopGameComponent() == null)
-                return false;
-
-            //if (AkiBackendCommunication.Instance.HighPingMode && ____player.IsYourPlayer)
-            //{
-            //    return true;
-            //}
-
-            var player = ____player;
-            if (player == null)
-                return false;
-
-            var result = false;
-            if (CallLocally.Contains(player.ProfileId))
-                result = true;
-
-            return result;
+            return true;
         }
 
         [PatchPostfix]
@@ -58,52 +38,22 @@ namespace StayInTarkov.Coop.Player.FirearmControllerPatches
             //GetLogger(typeof(FirearmController_Pickup_Patch)).LogDebug("PostPatch");
 
             var player = ____player;
-            if (CallLocally.Contains(player.ProfileId))
-            {
-                CallLocally.Remove(player.ProfileId);
-                return;
-            }
 
-            FCPickupPicket pickupPicket = new(player.ProfileId, p);
+            FCPickupPacket pickupPicket = new(player.ProfileId, p);
             GameClient.SendData(pickupPicket.Serialize());
-
-
-
         }
 
         public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
         {
-            //Logger.LogDebug("FirearmController_Pickup_Patch.Replicated");
-
-            if (AkiBackendCommunication.Instance.HighPingMode && player.IsYourPlayer)
-            {
-                // You would have already run this. Don't bother
-                return;
-            }
-
-            FCPickupPicket pp = new(null, false);
+            FCPickupPacket pp = new(null, false);
             pp.Deserialize((byte[])dict["data"]);
-
-            if (HasProcessed(GetType(), player, pp))
-                return;
 
             if (player.HandsController is EFT.Player.FirearmController firearmCont && pp.Pickup)
             {
-                CallLocally.Add(pp.ProfileId);
-                firearmCont.Pickup(pp.Pickup);
+                firearmCont.CurrentOperation.Pickup(pp.Pickup);
             }
         }
 
-        public class FCPickupPicket : BasePlayerPacket
-        {
-            public bool Pickup { get; set; }
-
-            public FCPickupPicket(string profileId, bool pickup) : base(profileId, "FCPickup")
-            {
-                Pickup = pickup;
-            }
-
-
-        }
+       
     }
 }
