00a13ec81d2b5bc7ea9474e8cd2c92307d3c4a6e
Paulov <pargsoft@hotmail.com>
2024-02-26 21:53:06 +0000

    Move and create GrenadeThrowPacket

--- Source/Coop/NetworkPacket/GrenadeThrowPacket.cs
+++ Source/Coop/NetworkPacket/GrenadeThrowPacket.cs
@@ -1,4 +1,7 @@
-﻿namespace StayInTarkov.Coop.NetworkPacket
+﻿using System;
+using System.IO;
+
+namespace StayInTarkov.Coop.NetworkPacket
 {
     public class GrenadeThrowPacket : BasePlayerPacket
     {
@@ -6,10 +9,36 @@
 
         public float rY { get; set; }
 
+        public GrenadeThrowPacket() { }
+
         public GrenadeThrowPacket(string profileId, UnityEngine.Vector2 rotation, string method) : base(profileId, method)
         {
             rX = rotation.x;
             rY = rotation.y;
+        }
+
+        public override byte[] Serialize()
+        {
+            var ms = new MemoryStream();
+            using BinaryWriter writer = new BinaryWriter(ms);
+            WriteHeaderAndProfileId(writer);
+            writer.Write(rX);
+            writer.Write(rY);
+
+            return ms.ToArray();
+        }
+
+        public override ISITPacket Deserialize(byte[] bytes)
+        {
+            if (bytes == null)
+                throw new ArgumentNullException(nameof(bytes));
+
+            using BinaryReader reader = new BinaryReader(new MemoryStream(bytes));
+            ReadHeaderAndProfileId(reader);
+            rX = reader.ReadSingle();
+            rY = reader.ReadSingle();
+
+            return this;
         }
     }
 }
