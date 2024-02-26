9f10eaab1f440e40135771299ae311ee474d0628
Paulov <pargsoft@hotmail.com>
2024-02-26 21:25:19 +0000

    Fix VersionChecks numbers

--- Source/StayInTarkovPlugin.cs
+++ Source/StayInTarkovPlugin.cs
@@ -274,11 +274,11 @@ namespace StayInTarkov
                     {
                         var majorN1 = EFTVersionMajor.Split('.')[0]; // 0
                         var majorN2 = EFTVersionMajor.Split('.')[1]; // 14
-                        var majorN3 = EFTVersionMajor.Split('.')[2]; // 0
-                        var majorN4 = EFTVersionMajor.Split('.')[3]; // 2
+                        var majorN3 = EFTVersionMajor.Split('.')[2]; // 1
+                        var majorN4 = EFTVersionMajor.Split('.')[3]; // 1
                         var majorN5 = EFTVersionMajor.Split('.')[4]; // build number
 
-                        if (majorN1 != "0" || majorN2 != "14" || majorN3 != "0" || majorN4 != "2")
+                        if (majorN1 != "0" || majorN2 != "14" || majorN3 != "1" || majorN4 != "1")
                         {
                             Logger.LogError("Version Check: This version of SIT is not designed to work with this version of EFT.");
                         }
