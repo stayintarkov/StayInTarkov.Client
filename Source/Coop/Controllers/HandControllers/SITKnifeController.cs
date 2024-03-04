986a7c12453b06c21a55691c5805eb06f7534139
Paulov <pargsoft@hotmail.com>
2024-03-04 17:15:30 +0000

    Add custom KnifeControllers

---
+++ Source/Coop/Controllers/HandControllers/SITKnifeController.cs
@@ -0,0 +1,75 @@
+﻿using BepInEx.Logging;
+using Comfort.Common;
+using EFT.InventoryLogic;
+using EFT.UI;
+using StayInTarkov.Coop.NetworkPacket.Player.Weapons;
+using StayInTarkov.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayInTarkov.Coop.Controllers.HandControllers
+{
+    public sealed class SITKnifeController : EFT.Player.KnifeController
+    {
+        ManualLogSource BepInLogger = BepInEx.Logging.Logger.CreateLogSource(nameof(SITKnifeController));
+
+        public override void Spawn(float animationSpeed, Action callback)
+        {
+            ConsoleScreen.Log($"{this.GetType().Name}:{nameof(Spawn)}");
+            BepInLogger.LogDebug($"{nameof(SITKnifeController)}:{nameof(Spawn)}");
+            base.Spawn(animationSpeed, callback);
+        }
+
+        public override void Execute(IOperation1 operation, Callback callback)
+        {
+            BepInLogger.LogDebug($"{nameof(SITKnifeController)}:{nameof(Execute)}");
+            base.Execute(operation, callback);
+        }
+
+        public override void Drop(float animationSpeed, Action callback, bool fastDrop = false, Item nextControllerItem = null)
+        {
+            base.Drop(animationSpeed, callback, fastDrop, nextControllerItem);
+            BepInLogger.LogDebug($"{nameof(SITKnifeController)}:{nameof(Drop)}");
+        }
+
+        public override void BrakeCombo()
+        {
+            base.BrakeCombo();
+        }
+
+        public override void ContinueCombo()
+        {
+            base.ContinueCombo();
+        }
+
+        public override void ExamineWeapon()
+        {
+            ExamineWeaponPacket packet = new(_player.ProfileId);
+            GameClient.SendData(packet.Serialize());
+            base.ExamineWeapon();
+        }
+
+        public override bool MakeAlternativeKick()
+        {
+            return base.MakeAlternativeKick();
+        }
+
+        public override bool MakeKnifeKick()
+        {
+            return base.MakeKnifeKick();
+        }
+
+        public override void Loot(bool p)
+        {
+            base.Loot(p);
+        }
+
+        public override void Pickup(bool p)
+        {
+            base.Pickup(p);
+        }
+    }
+}
---
+++ Source/Coop/Controllers/HandControllers/SITQuickKnifeKickController.cs
@@ -0,0 +1,49 @@
+﻿using BepInEx.Logging;
+using Comfort.Common;
+using EFT.InventoryLogic;
+using EFT.UI;
+using StayInTarkov.Coop.NetworkPacket.Player.Weapons;
+using StayInTarkov.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EFT.Player;
+
+namespace StayInTarkov.Coop.Controllers.HandControllers
+{
+    public class SITQuickKnifeKickController : QuickKnifeKickController
+    {
+        ManualLogSource BepInLogger = BepInEx.Logging.Logger.CreateLogSource(nameof(SITQuickKnifeKickController));
+
+        public override void Spawn(float animationSpeed, Action callback)
+        {
+            ConsoleScreen.Log($"{this.GetType().Name}:{nameof(Spawn)}");
+            BepInLogger.LogDebug($"{nameof(SITQuickKnifeKickController)}:{nameof(Spawn)}");
+            base.Spawn(animationSpeed, callback);
+        }
+
+        public override void Execute(IOperation1 operation, Callback callback)
+        {
+            BepInLogger.LogDebug($"{nameof(SITQuickKnifeKickController)}:{nameof(Execute)}");
+            base.Execute(operation, callback);
+        }
+
+        public override void Drop(float animationSpeed, Action callback, bool fastDrop = false, Item nextControllerItem = null)
+        {
+            base.Drop(animationSpeed, callback, fastDrop, nextControllerItem);
+            BepInLogger.LogDebug($"{nameof(SITQuickKnifeKickController)}:{nameof(Drop)}");
+        }
+
+        public override void Loot(bool p)
+        {
+            base.Loot(p);
+        }
+
+        public override void Pickup(bool p)
+        {
+            base.Pickup(p);
+        }
+    }
+}
