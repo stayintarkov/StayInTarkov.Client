22244ef3c1e4cf802762de9159d0fd0cebfec4ef
Paulov <pargsoft@hotmail.com>
2024-03-07 13:05:43 +0000

    Fix QuickKnife

--- Source/Coop/Controllers/HandControllers/SITQuickKnifeKickController.cs
+++ Source/Coop/Controllers/HandControllers/SITQuickKnifeKickController.cs
@@ -20,7 +20,6 @@ namespace StayInTarkov.Coop.Controllers.HandControllers
 
         public override void Spawn(float animationSpeed, Action callback)
         {
-            ConsoleScreen.Log($"{this.GetType().Name}:{nameof(Spawn)}");
             BepInLogger.LogDebug($"{nameof(SITQuickKnifeKickController)}:{nameof(Spawn)}");
             base.Spawn(animationSpeed, callback);
         }
