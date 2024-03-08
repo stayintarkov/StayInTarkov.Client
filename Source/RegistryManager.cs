3ee4bcf1bfa1f017a0a6ada4dcf328eefe5d3352
Paulov <pargsoft@hotmail.com>
2024-03-08 08:44:57 +0000

    Add detection of legal copy using InstallLocation and Log it to output

--- Source/RegistryManager.cs
+++ Source/RegistryManager.cs
@@ -1,23 +1,45 @@
-﻿using Microsoft.Win32;
+﻿using BepInEx.Logging;
+using Microsoft.Win32;
+using System.IO;
 
 namespace StayInTarkov
 {
     public static class RegistryManager
     {
+        public static ManualLogSource Logger { get; } = BepInEx.Logging.Logger.CreateLogSource($"{nameof(RegistryManager)}");
         public static string GamePathEXE
         {
             get
             {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"))
+                {
+                    if (key != null)
+                    {
+                        string installPath = key.GetValue("InstallLocation")?.ToString();
+
+                        if (!string.IsNullOrEmpty(installPath) && File.Exists(Path.Combine(installPath, "EscapeFromTarkov.exe")))
+                        {
+                            var exePath = Path.Combine(installPath, "EscapeFromTarkov.exe");
+                            Logger.LogInfo($"{nameof(RegistryManager)}:Found Exe {exePath} using InstallLocation key");
+                            return exePath;
+                        }
+                    }
+                }
 
                 using (RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\EscapeFromTarkov"))
                 {
                     if (key != null)
                     {
-                        string exePath = key.GetValue("DisplayIcon").ToString();
-                        return exePath;
+                        string exePath = key.GetValue("DisplayIcon")?.ToString();
+                        if (!string.IsNullOrEmpty(exePath))
+                        {
+                            Logger.LogInfo($"{nameof(RegistryManager)}:Found Exe {exePath} using DisplayIcon key");
+                            return exePath;
+                        }
                     }
                 }
 
+                
                 return string.Empty;
             }
         }
