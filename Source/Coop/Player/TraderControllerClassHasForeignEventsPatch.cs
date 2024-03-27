b6f4960633e8b776c30ffc372cf4e6ceba08ad95
Paulov <pargsoft@hotmail.com>
2024-03-27 18:53:02 +0000

    Fix "HasForeignEvents" method when unloading bullets

---
+++ Source/Coop/Player/TraderControllerClassHasForeignEventsPatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayInTarkov.Coop.Player
+{
+    internal sealed class TraderControllerClassHasForeignEventsPatch : ModuleReplicationPatch
+    {
+        public override Type InstanceType => typeof(TraderControllerClass);
+
+        public override string MethodName => "HasForeignEvents";
+
+        public override void Replicated(EFT.Player player, Dictionary<string, object> dict)
+        {
+        }
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return ReflectionHelpers.GetMethodForType(InstanceType, MethodName);
+        }
+
+        [PatchPrefix]
+        public static bool Prefix(bool __result)
+        {
+            __result = false;
+            return false;
+        }
+    }
+}
