e3af89bfe63674b2d1749e455282ffb737d1f1ee
JimWails <1142247734@qq.com>
2024-04-01 03:56:30 +0800

    New bundle system support

--- Source/EssentialPatches/Bundles/EasyBundlePatch.cs
+++ Source/EssentialPatches/Bundles/EasyBundlePatch.cs
@@ -1,6 +1,7 @@
 ﻿using Aki.Custom.Models;
 using Aki.Custom.Utils;
 using Diz.DependencyManager;
+using System;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -31,24 +32,26 @@ namespace StayInTarkov
         [PatchPostfix]
         private static void PatchPostfix(object __instance, string key, string rootPath, CompatibilityAssetBundleManifest manifest, IBundleLock bundleLock)
         {
-            var path = rootPath + key;
-            var dependencyKeys = manifest.GetDirectDependencies(key) ?? new string[0];
+            var filepath = rootPath + key;
+            var dependencies = manifest.GetDirectDependencies(key) ?? Array.Empty<string>();
 
-            if (BundleManager.Bundles.TryGetValue(key, out BundleInfo bundle))
+            if (BundleManager.Bundles.TryGetValue(key, out BundleItem bundle))
             {
-                Logger.LogInfo(path);
-                dependencyKeys = (dependencyKeys.Length > 0) ? dependencyKeys.Union(bundle.DependencyKeys).ToArray() : bundle.DependencyKeys;
-                path = bundle.Path;
-                Logger.LogInfo(path);
+                // server bundle
+                dependencies = (dependencies.Length > 0)
+                    ? dependencies.Union(bundle.Dependencies).ToArray()
+                    : bundle.Dependencies;
 
+                // set path to either cache (HTTP) or mod (local)
+                filepath = BundleManager.GetBundlePath(bundle);
             }
 
             _ = new EasyBundleHelper(__instance)
             {
                 Key = key,
-                Path = path,
+                Path = filepath,
                 KeyWithoutExtension = Path.GetFileNameWithoutExtension(key),
-                DependencyKeys = dependencyKeys,
+                DependencyKeys = dependencies,
                 LoadState = new BindableState<ELoadState>(ELoadState.Unloaded, null),
                 BundleLock = bundleLock
             };
