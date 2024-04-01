69e166e5af1eba2219851c4026239a05da04d761
Paulov <pargsoft@hotmail.com>
2024-04-01 11:54:25 +0100

    comment top of file

--- Source/Coop/NetworkPacket/Player/Weapons/InitiateShotPacket.cs
+++ Source/Coop/NetworkPacket/Player/Weapons/InitiateShotPacket.cs
@@ -1,4 +1,9 @@
-﻿using BepInEx.Logging;
+﻿/**
+ * This file is written and licensed by Paulov (https://github.com/paulov-t) for Stay in Tarkov (https://github.com/stayintarkov)
+ * You are not allowed to reproduce this file in any other project
+ */
+
+using BepInEx.Logging;
 using EFT;
 using EFT.InventoryLogic;
 using EFT.UI;
