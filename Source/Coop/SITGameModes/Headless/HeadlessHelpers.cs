237dc93f8c96c79abed7a377ace61c419a58d051
Paulov <pargsoft@hotmail.com>
2024-04-02 18:08:54 +0100

    Add a Helper to start a raid from any other part of the project

---
+++ Source/Coop/SITGameModes/Headless/HeadlessHelpers.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx.Logging;
+using Comfort.Common;
+using EFT;
+using StayInTarkov.Coop.Matchmaker;
+using StayInTarkov.Networking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StayInTarkov.Coop.SITGameModes.Headless
+{
+    public static class HeadlessHelpers
+    {
+        static HeadlessHelpers()
+        {
+            Logger = BepInEx.Logging.Logger.CreateLogSource(nameof(HeadlessHelpers));
+        }
+
+        public static ManualLogSource Logger { get; }
+
+        public static void StartGame(string locationName)
+        {
+            if (Singleton<ClientApplication<ISession>>.Instantiated)
+            {
+                Logger.LogDebug($"Force start of game in Headless!");
+
+                var tarkovApp = Singleton<ClientApplication<ISession>>.Instance as TarkovApplication;
+                SITMatchmaking.MatchingType = EMatchmakerType.HeadlessHost;
+                tarkovApp.InternalStartGame(locationName, true, true);
+                var raidSettings = (RaidSettings)ReflectionHelpers.GetFieldFromTypeByFieldType(tarkovApp.GetType(), typeof(RaidSettings)).GetValue(tarkovApp);
+                SITMatchmaking.CreateMatch(
+                   tarkovApp.Session.Profile.ProfileId
+                   , raidSettings
+                   , ""
+                   , ESITProtocol.PeerToPeerUdp
+                   , null
+                   , 6972
+                   , EMatchmakerType.HeadlessHost);
+            }
+        }
+    }
+}
