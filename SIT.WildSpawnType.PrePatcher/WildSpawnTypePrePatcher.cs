f81299b2fc3e472b602bfae5f93937365e8e6843
Paulov <pargsoft@hotmail.com>
2024-05-16 22:24:02 +0100

    Update sptUsec and sptBear for 3.9

--- SIT.WildSpawnType.PrePatcher/WildSpawnTypePrePatcher.cs
+++ SIT.WildSpawnType.PrePatcher/WildSpawnTypePrePatcher.cs
@@ -1,6 +1,6 @@
+using Mono.Cecil;
 using System.Collections.Generic;
 using System.Linq;
-using Mono.Cecil;
 
 namespace StayInTarkov
 {
@@ -8,8 +8,8 @@ namespace StayInTarkov
     {
         public static IEnumerable<string> TargetDLLs { get; } = new[] { "Assembly-CSharp.dll" };
 
-        public static int sptUsecValue = 47;
-        public static int sptBearValue = 48;
+        public static int sptUsecValue = 100;
+        public static int sptBearValue = 101;
 
         public static void Patch(ref AssemblyDefinition assembly)
         {
@@ -18,17 +18,17 @@ namespace StayInTarkov
             var sptUsec = new FieldDefinition("sptUsec",
                     FieldAttributes.Public | FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.HasDefault,
                     botEnums)
-                { Constant = sptUsecValue };
+            { Constant = sptUsecValue };
 
             var sptBear = new FieldDefinition("sptBear",
                     FieldAttributes.Public | FieldAttributes.Static | FieldAttributes.Literal | FieldAttributes.HasDefault,
                     botEnums)
-                { Constant = sptBearValue };
+            { Constant = sptBearValue };
 
-            if(!botEnums.Fields.Any(x => x.Name == "sptUsec"))
+            if (!botEnums.Fields.Any(x => x.Name == "sptUsec"))
                 botEnums.Fields.Add(sptUsec);
 
-            if(!botEnums.Fields.Any(x => x.Name == "sptBear"))
+            if (!botEnums.Fields.Any(x => x.Name == "sptBear"))
                 botEnums.Fields.Add(sptBear);
         }
     }
